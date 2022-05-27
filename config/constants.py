271e4c6419c5067a4d2da925d1b074a002327f83
Lucas Noga <luc4snoga@gmail.com>
2022-05-27 13:04:31 +0200

    Version v1.1.0

--- config/constants.py
+++ config/constants.py
@@ -1,7 +1,7 @@
 '''Constants for Dathomir projects'''
 
 PROJECT: str = 'Dathomir'
-VERSION: str = 'v1.0.0'
+VERSION: str = 'v1.1.0'
 CONFIG_PATH: str = 'config.json'
 DEFAULT_DEBUG: str = 'true'
 DEFAULT_REPOSITORY: str = 'repositories'
